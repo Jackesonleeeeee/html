ed4a891c31663d5ede2d2411d976dafc97d3953d
Jacksonleeeeee <jacksonli1007@gmail.com>
2021-10-23 19:40:03 -0700

    first

---
+++ controll.js
@@ -0,0 +1,8 @@
+main=document.getElementById('cat')
+main.addEventListener('click',function(){
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+})
+
+document.getElementById('nextpage').addEventListener('click', function(){
+    window.location = 'new.html';
+})
